29a49791e8705116480eeb5ca583ebd212b63ef1
nexusuw <s3ramsay@riseup.net>
2016-07-23 14:35:08 -0400

    able to record saves

--- src/extension.ts
+++ src/extension.ts
@@ -3,25 +3,46 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
+function run_cmd(cmd, args, callBack) {
+      var spawn = require('child_process').spawn;
+      var child = spawn(cmd, args);
+      var resp = "";
+      child.on('error', function (code) { console.log('error');callBack(code) });
+
+      child.stdout.on('data', function (buffer) { resp += buffer.toString() });
+      child.on('close', function (code) {  console.log(resp); callBack(code) });
+
+    }
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-gtm" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "vscode-gtm" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+  // check gtm exists + initd/
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+      run_cmd('gtm',[], (res) => {
+        if (res < 0){
+          vscode.window.showErrorMessage('gtm is not avaliable on your $PATH. please install it first');
+        }
+      });
 
-    context.subscriptions.push(disposable);
+
+
+  //}
+
+  let subscriptions: vscode.Disposable[] = [];
+
+  vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
+
+
+    run_cmd('gtm', ['record', e.fileName], (res) => console.log(res))
+
+  }, this, subscriptions);
 }
 
 // this method is called when your extension is deactivated
